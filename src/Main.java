610134e87aaab9c8b5f9998749580645d5106215
mioamio <zwv0101@gmail.com>
2022-09-04 11:31:44 +0300

    add modern

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int ticketPrice = 23_500;
-        int bonusTrack = 20;
-        int MilesBonus = ticketPrice / bonusTrack;
-
-        System.out.println("При покупке билета стоимостью " + ticketPrice + "руб., Ваш бонус составит " +MilesBonus + " бонусных миль.");
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
